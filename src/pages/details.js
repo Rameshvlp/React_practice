477af137e79326bf2cf1a93f8fa1aea0267c2a96
Rameshvlp <rameshvlp2001@gmail.com>
2023-12-09 00:24:04 +0530

    Api learning

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import Navbar from '../components/navbar'
+import React, { useState } from 'react';
+import Navbar from '../components/navbar';
 
-function details() {
+const Details = () => {
+  const [names, setName] = useState("");
+  const [displayedName, setDisplayedName] = useState(""); 
+
+  const handleClick = () => {
+    setDisplayedName(names); 
+  };
+
   return (
     <div>
-    <Navbar />
-    <h1>Details page</h1>
-
-</div>
-  )
+      <Navbar />
+      <h1>Details page</h1>
+      <div>
+        <form>
+          Name
+          <input type='text' value={names} onChange={(e) => setName(e.target.value)} />
+        </form>
+        <button onClick={handleClick}>Click</button>
+        {/* Display the name */}
+        {displayedName && <h1>{displayedName}</h1>}
+      </div>
+    </div>
+  );
 }
 
-export default details
+export default Details;
--- src/pages/place.js
+++ src/pages/place.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/navbar';
-const place = () =>{
-  console.log('hello')
+import Axios from 'axios';
+
+const App = () => {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState();
+
+  const fetchData = async () => {
+    try {
+      const response = await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=edec7f1a9d3bfc829349a89291ef69bf`);
+      setData(response.data);
+    } catch (err) {
+      alert("Enter City Name ");
+    }
+  };
+
   return (
-    
     <div>
-      <Navbar />  
-      <h1>place page</h1>
-     <form>Enter your name: 
-      <input type='text'/>
-     </form><form>Enter your number: 
-      <input type='number'/>
-     </form>
-     <form> 
-     </form>
-     <form>hobbies: 
-      <input type='checkbox'/>
-      <input type='checkbox'/>
-      <input type='checkbox'/>
+      <Navbar />
+      <h1>weather check using api</h1>
+      <form>
+        Enter City Name :
+        <input
+          type='text'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder='Enter City Name'
+        />
+      </form>
 
-     </form>
-
+      <button onClick={fetchData}>GET WEATHER</button>
+      
+      {data && (
+        <div>
+          <h1>{data.name}, {data.sys.country}</h1>
+          <div>{Math.round(data.main?.temp)} C</div>
+          <div>Lat - {data.coord?.lat}</div>
+          <div>Lon - {data.coord?.lon}</div>
+        </div>
+      )}
     </div>
   );
-}
-export default place;
+};
+
+export default App;
+ 
